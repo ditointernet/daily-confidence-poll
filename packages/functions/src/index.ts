8fa625f4b03a4205141b61e8bcd95d48061d486f
Uriell <me+github@uriell.dev>
2022-06-26 17:33:15 -0300

    adding single function updating vote statuses

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,9 +1,34 @@
+import { firestore } from "firebase-admin";
+import { initializeApp } from "firebase-admin/app";
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+initializeApp();
+const db = firestore();
+const pollsCollection = db.collection("polls");
+
+interface EnhancedEventContext extends functions.EventContext {
+  auth?: {
+    uid: string;
+    token: {
+      email?: string;
+    };
+  };
+}
+
+export const onUserVoted = functions
+  .region("southamerica-east1")
+  .firestore.document("polls/{pollId}/votes/{voteId}")
+  .onCreate(function onVoteDocumentCreated(_, context: EnhancedEventContext) {
+    const pollId = context.params.pollId as string;
+    const voteId = context.params.voteId as string;
+    const poll = pollsCollection.doc(pollId);
+
+    return poll.update(
+      {
+        hasParticipantVoted: {
+          [voteId]: true,
+        },
+      },
+      { exists: true }
+    );
+  });
