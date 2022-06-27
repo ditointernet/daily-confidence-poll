4c4e810b6f8bbfd13c138b337d4e100979f25558
Uriell <me+github@uriell.dev>
2022-06-27 12:30:36 -0300

    implementing redirect after login

--- packages/hosting/src/containers/middlewares/AuthenticatedRoute.tsx
+++ packages/hosting/src/containers/middlewares/AuthenticatedRoute.tsx
@@ -1,16 +1,25 @@
 import ConditionalRoute from "./ConditionalRoute";
 import { useFirebaseAuthUser } from "../AuthProvider";
 import { AUTH_LOGIN } from "../../constants/routes";
+import { useLocation } from "react-router-dom";
 
 const AuthenticatedRoute: React.FC = () => {
   const firebaseAuthUser = useFirebaseAuthUser();
+  const location = useLocation();
 
   function checkIfAuthenticated() {
     return !!firebaseAuthUser;
   }
 
+  const redirectAfterLogin = encodeURIComponent(
+    location.pathname + location.search
+  );
+
   return (
-    <ConditionalRoute redirect={AUTH_LOGIN} condition={checkIfAuthenticated} />
+    <ConditionalRoute
+      redirect={AUTH_LOGIN + `?redirectAfterLogin=${redirectAfterLogin}`}
+      condition={checkIfAuthenticated}
+    />
   );
 };
 
--- packages/hosting/src/pages/Login.tsx
+++ packages/hosting/src/pages/Login.tsx
@@ -1,17 +1,23 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 import { HOME } from "../constants/routes";
 
 const Login: React.FC = () => {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [search] = useSearchParams();
+  const redirectAfterLogin = search.get("redirectAfterLogin");
 
   function onGoogleLoginClick() {
     const provider = new GoogleAuthProvider();
 
     return signInWithPopup(auth, provider)
-      .then(() => navigate(HOME))
+      .then(() =>
+        navigate(
+          redirectAfterLogin ? decodeURIComponent(redirectAfterLogin) : HOME
+        )
+      )
       .catch(console.error);
   }
 
