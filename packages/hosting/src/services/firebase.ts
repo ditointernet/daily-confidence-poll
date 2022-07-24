ba92351e680524876b54b90e413f611fa127a53b
Uriell <me+github@uriell.dev>
2022-07-24 17:24:24 -0300

    moving build envs and rewriting process.env usage

--- packages/hosting/src/services/firebase.ts
+++ packages/hosting/src/services/firebase.ts
@@ -1,40 +1,42 @@
 import { FirebaseOptions, initializeApp } from "firebase/app";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 
+const env = process.env;
+
 if (
-  !process.env.REACT_APP_FIREBASE_PROJECT_ID ||
-  !process.env.REACT_APP_FIREBASE_MEASUREMENT_ID ||
-  !process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ||
-  !process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ||
-  !process.env.REACT_APP_FIREBASE_API_KEY ||
-  !process.env.REACT_APP_FIREBASE_APP_ID
+  !env.REACT_APP_FIREBASE_PROJECT_ID ||
+  !env.REACT_APP_FIREBASE_MEASUREMENT_ID ||
+  !env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ||
+  !env.REACT_APP_FIREBASE_AUTH_DOMAIN ||
+  !env.REACT_APP_FIREBASE_API_KEY ||
+  !env.REACT_APP_FIREBASE_APP_ID
 ) {
   throw new Error("unset environment variables: firebaseConfig");
 }
 
 const firebaseConfig: FirebaseOptions = {
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  projectId: env.REACT_APP_FIREBASE_PROJECT_ID,
+  measurementId: env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  authDomain: env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  apiKey: env.REACT_APP_FIREBASE_API_KEY,
+  appId: env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default function initFirebase() {
-  if (!process.env.REACT_APP_FIREBASE_FIRESTORE_EMULATOR_PORT) {
+  initializeApp(firebaseConfig);
+
+  if (env.NODE_ENV === "production") return;
+
+  if (!env.REACT_APP_FIREBASE_FIRESTORE_EMULATOR_PORT) {
     throw new Error(
       "unset environment variables: FIREBASE_FIRESTORE_EMULATOR_PORT"
     );
   }
 
-  initializeApp(firebaseConfig);
-
-  if (process.env.NODE_ENV === "development") {
-    connectFirestoreEmulator(
-      getFirestore(),
-      "localhost",
-      parseInt(process.env.REACT_APP_FIREBASE_FIRESTORE_EMULATOR_PORT)
-    );
-  }
+  connectFirestoreEmulator(
+    getFirestore(),
+    "localhost",
+    parseInt(env.REACT_APP_FIREBASE_FIRESTORE_EMULATOR_PORT)
+  );
 }
