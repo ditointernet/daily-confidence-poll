ca3e396667abd4740366e8326aea6afabd6bc639
roger067 <rosestolato.rogerio@gmail.com>
2022-10-21 18:10:14 -0300

    adding dito-uai and edit home page visual

--- packages/hosting/src/index.tsx
+++ packages/hosting/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
 import Router from "./Router";
 import initFirebase from "./services/firebase";
@@ -8,8 +9,15 @@ import reportWebVitals from "./reportWebVitals";
 
 initFirebase();
 
+const GlobalStyle = createGlobalStyle`
+  #root {
+    height: 100%;
+  }
+`;
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
     <BrowserRouter>
       <Router />
     </BrowserRouter>
