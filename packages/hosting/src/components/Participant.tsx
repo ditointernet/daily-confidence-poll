eadc8aeb48f2ddb055491f03adc98a660b9f379d
rogerioDito <rogerio.moura@dito.com.br>
2023-06-01 23:35:44 -0300

    update poll page

--- packages/hosting/src/components/Participant.tsx
+++ packages/hosting/src/components/Participant.tsx
@@ -1,4 +1,12 @@
+import { Card, Flex, Text } from "@ditointernet/uai-components";
 import { VoteRange } from "../constants/types";
+import { COLORS, GRID, TYPOGRAPHY } from "@ditointernet/uai-foundation";
+
+const confidence = {
+  1: "Baixa 😨",
+  2: "Média 🤨",
+  3: "Alta 😎",
+};
 
 type ParticipantProps = {
   participantId: string;
@@ -15,13 +23,32 @@ const Participant: React.FC<ParticipantProps> = ({
   hasVoted,
   vote,
 }) => (
-  <div style={{ display: "inline-block", marginLeft: "1rem" }}>
-    <h1>{displayName || participantId}</h1>
-    {!!photoUrl && <img src={photoUrl} alt={`${displayName}'s face`} />}
-    <p>
-      {!!vote ? "Voto:" : "Votou?"} {!!vote ? vote : hasVoted ? "Sim" : "Não"}
-    </p>
-  </div>
+  <Card padding={GRID(2)}>
+    <Flex alignItems="center">
+      {!!photoUrl && (
+        <img
+          src={photoUrl}
+          alt={`${displayName}'s face`}
+          width={100}
+          height={100}
+          style={{ borderRadius: "50%" }}
+        />
+      )}
+      <Flex ml={GRID(2)} flexDirection="column">
+        <Text
+          weight={TYPOGRAPHY.FontWeight.SEMI_BOLD}
+          size={TYPOGRAPHY.FontSize.LARGE}
+          color={COLORS.GRAY_6}
+        >
+          {displayName || participantId}
+        </Text>
+        <Text size={TYPOGRAPHY.FontSize.SMALL} color={COLORS.GRAY_5}>
+          {!!vote ? "Confiança:" : "Votou?"}{" "}
+          {!!vote ? confidence[vote] : hasVoted ? "Sim" : "Não"}
+        </Text>
+      </Flex>
+    </Flex>
+  </Card>
 );
 
 export default Participant;
